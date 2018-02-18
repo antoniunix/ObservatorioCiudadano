e7400de4497eaf2540c6ece0220447915c5deb0d
gnu <antonio.mendoza@go-sharp.net>
2018-02-17 21:37:06 -0600

    map

--- app/src/main/java/net/gshp/observatoriociudadano/geolocation/ServicesGeolocation.java
+++ app/src/main/java/net/gshp/observatoriociudadano/geolocation/ServicesGeolocation.java
@@ -36,7 +36,7 @@ import java.util.Map;
 public class ServicesGeolocation extends IntentService implements OnApiGeolocation {
     private final int ENVIOFINALIZADO = -1000;
     private final int SINREPORTES = -2000;
-    private DtoReportGeolocation dto;
+    private List<DtoReportGeolocation> dto;
     private DaoReportGeolocation daoReportGeolocation;
     private Geolocation geolocation;
     private NetworkConfig networkConfig;
@@ -125,44 +125,12 @@ public class ServicesGeolocation extends IntentService implements OnApiGeolocati
             if (completedTask != null && (completedTask.getTag().equals("geo"))) {
 
                 Log.i("geo", "status GEO" + completedTask.getResponseStatus());
-                if (completedTask.getResponseStatus() == HttpStatus.SC_CREATED) {
-                    new DaoReportGeolocation().deleteById(dto.getId());
-                    dto = daoReportGeolocation.Select();
-                    if (dto.getId() > 0) {
-                        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
-                        nameValuePairs.add(new BasicNameValuePair("json", new Gson().toJson(dto)));
-                        //asegurarnos de no mandar datos null
-                        if (dto.getTz() != null)
-                            networkConfig.POST_GEO("geo", nameValuePairs, "geo");
-                    }
+                if (completedTask.getResponseStatus() == HttpStatus.SC_CREATED||completedTask.getResponseStatus() == HttpStatus.SC_OK) {
+                    new DaoReportGeolocation().delete();
                 }
                 stopSelf();
             }
         }
     }
 
-    class HandlerTaskReportsSend extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case SINREPORTES:
-                    Log.e("ServiceGeolocation:211", "Sin reportes a enviar");
-                    break;
-                case ENVIOFINALIZADO:
-                    Log.e("ServiceGeolocation:214", "Reportes enviados");
-                    break;
-                case HttpStatus.SC_CONFLICT:
-                    Log.e("ServiceGeolocation:217", "No se pudieron enviar sus reportes,\\nintentelo más tarde");
-                    break;
-                case HttpStatus.SC_FORBIDDEN:
-                    Log.e("ServiceGeolocation:217", "El usuario que ingresó tiene estatus inactivo.\\n No puede hacer uso de la aplicación, para mayor información consultelo con su supervisor.");
-                    break;
-                case HttpStatus.SC_UNAUTHORIZED:
-                    Log.e("ServiceGeolocation:217", "Su nombre de usuario y/o contraseña son incorrectos");
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
 }
